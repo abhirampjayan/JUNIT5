6901e336e8840b080f7ae1c15531711530b91883
Abhiram P <abhiram.p@ecstvm.local>
2020-10-04 23:10:42 +0530

    assert exceptions

--- src/test/java/org/example/MultiTest.java
+++ src/test/java/org/example/MultiTest.java
@@ -1,11 +1,17 @@
 package org.example;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MultiTest {
-    Multi multi=new Multi();
+    Multi multi;
+    @BeforeEach
+    void init(){
+        multi=new Multi();
+    }
     @Test
     void add() {
 
